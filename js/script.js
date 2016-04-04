f41c158851e0605a9af89e28c91914b0e1185d5e
vluna <vluna.0293@gmail.com>
2016-04-03 22:39:51 -0700

    Fixed values communication with front end

--- js/script.js
+++ js/script.js
@@ -47,22 +47,22 @@ setInterval(function(){
 	//}, 1);
 }, 1);
 
-function loadJSON(callback) {
+// function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+//     var xobj = new XMLHttpRequest();
+//     xobj.overrideMimeType("application/json");
+//     xobj.open('GET', 'data.json', true);
+//     xobj.onreadystatechange = function() {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
 
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
+//             // .open will NOT return a value but simply returns undefined in async mode so use a callback
+//             callback(xobj.responseText);
 
-        }
-    }
-    xobj.send(null);
+//         }
+//     }
+//     xobj.send(null);
 
-}
+// }
 
 // setInterval(function() {
 //     $.getJSON('data.json', function(data) {
--- main.js
+++ main.js
@@ -1,66 +1,62 @@
 // Dependencies
 var five = require("johnny-five");
-var mysql = require('mysql');
-
-var jsonfile = require('jsonfile')
-var util = require('util')
- 
-var file = 'data.json'
-jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj.id)
-})
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var path = require("path");
 
 var board = new five.Board(); // Connect to arduino board
 
-var xCord;
-var yCord;
-// Database information
-var connection_mysql = mysql.createConnection({
-    host: 'ns8037.hostgator.com',
-    user: 'vluna_projects',
-    password: 'projects',
-    database: 'vluna_fruit_jedy'
+// Sensors
+var accelerometer;
+
+// Outputs
+var acc_output;
+
+// Start on local server
+app.listen(8000);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+// Set up the board
+board.on("ready", function() {
+    accelerometer = new five.Accelerometer({
+        controller: "ADXL345"
+    });
 });
 
-connection_mysql.connect(); // Connect to database
-board.on("ready", function() {
-  var accelerometer = new five.Accelerometer({
-    controller: "ADXL345"
-  });
+// Connect to server
+io.sockets.on('connection', function (socket) {
+    // When board is ready
+    if(board.isReady) {
+        // When accelerometer values change
+        accelerometer.on("change", function() {
 
+        // Accelerometer output
+        acc_output = { x: this.x,
+                       y: this.y,
+                       z: this.z,
+                       pitch: this.pitch,
+                       roll: this.roll,
+                       inclination: this.inclination,
+                       orientation: this.orientation,
+                       acceleration: this.acceleration
+                     }
 
-  accelerometer.on("change", function() {
-    // connection_mysql.query('UPDATE AccelerometerValues SET x = ?, y = ?, z = ?, pitch = ?, roll = ?, acceleration = ?, inclination = ?, orientation = ? Where ID = 1', [(this.x*100), this.y, this.z, this.pitch, this.roll, this.acceleration, this.inclination, this.orientation], function (err, result) {
-    // if (err) throw err;
-    // });
-
-
-    var obj = { id: "1",
-                x: this.x,
-                y: this.y,
-                z:"1",
-                pitch:"1",
-                roll:"1",
-                inclination:"1",
-                orientation:"1",
-                acceleration:"1"
-              }
-    jsonfile.writeFileSync(file, obj);
-
-    jsonfile.readFile(file, function(err, obj) {
-      console.dir(obj)
-    })
-
-  
-    // console.log("accelerometer");
-    // console.log("  x            : ", this.x);
-    // console.log("  y            : ", this.y);
-    // console.log("  z            : ", this.z);
-    // console.log("  pitch        : ", this.pitch);
-    // console.log("  roll         : ", this.roll);
-    // console.log("  acceleration : ", this.acceleration);
-    // console.log("  inclination  : ", this.inclination);
-    // console.log("  orientation  : ", this.orientation);
-    // console.log("--------------------------------------");
-  });
+            // Debug
+            // console.log(acc_output); 
+            // console.log("Sending...");    
+            socket.emit('news', acc_output); // Send information to client
+        });
+    }
 });
